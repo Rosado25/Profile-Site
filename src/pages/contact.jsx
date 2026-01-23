452fd258eb16f346493a3963f224817006cb3d91
Alexandre <alexandrerosado2004@gmail.com>
2026-01-23 13:25:36 +0100

    page contact warning

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -2,9 +2,11 @@
 
 export default function Contact() {
     return (
-      <section className="simple-section py-20">
-        <h1>Contact Page</h1>
-      </section>
+      <div className="simple-section">
+        <section className="contact">
+          <h1>EN CONSTRUCTION...</h1>
+        </section>
+      </div>
       );
     }
     
